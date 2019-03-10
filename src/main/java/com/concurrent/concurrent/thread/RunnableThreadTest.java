c732d724d03b81cc9bcc9b471b8123bdd8e2d64a
xingxingt <1039380441@qq.com>
2019-03-10 09:23:29 +0800

    commit code

---
+++ src/main/java/com/concurrent/concurrent/thread/RunnableThreadTest.java
@@ -0,0 +1,29 @@
+package com.concurrent.concurrent.thread;
+
+/**
+ * 使用runnable创建启动线程
+ */
+public class RunnableThreadTest implements Runnable {
+
+    private int i;
+
+    public void run() {
+        for (i = 0; i < 100; i++) {
+            System.out.println(Thread.currentThread().getName() + " " + i);
+        }
+    }
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            System.out.println(Thread.currentThread().getName() + " " + i);
+            if (i == 20) {
+                RunnableThreadTest rtt = new RunnableThreadTest();
+                new Thread(rtt, "新线程1").start();
+                new Thread(rtt, "新线程2").start();
+            }
+        }
+
+    }
+
+}
+
---
+++ src/main/java/com/concurrent/concurrent/thread/ThreadTest.java
@@ -0,0 +1,28 @@
+package com.concurrent.concurrent.thread;
+
+
+/**
+ * 使用Thread创建启动线程
+ */
+public class ThreadTest extends Thread {
+    int i = 0;
+
+    //重写run方法，run方法的方法体就是现场执行体
+    public void run() {
+        for (; i < 100; i++) {
+            System.out.println(getName() + "  " + i);
+
+        }
+    }
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            System.out.println(Thread.currentThread().getName() + "  : " + i);
+            if (i == 20) {
+                new ThreadTest().start();
+                new ThreadTest().start();
+            }
+        }
+    }
+
+}
