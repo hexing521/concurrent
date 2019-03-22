01f9531c23e88733f4c4600d95c898a12b15a354
xingxingt <1039380441@qq.com>
2019-03-23 07:56:09 +0800

    commit code

---
+++ src/main/java/com/concurrent/concurrent/threadpool/ThreadPoolRejectedDemo.java
@@ -0,0 +1,132 @@
+package com.concurrent.concurrent.threadpool;
+
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.RejectedExecutionException;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+class MyRunnable implements Runnable {
+    private String name;
+
+    public MyRunnable(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void run() {
+        try {
+            System.out.println(this.name + " is running.");
+            Thread.sleep(200);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+/**
+ * java线程池的拒绝策略的实现:
+ * 1,ThreadPoolExecutor.AbortPolicy:丢弃任务并抛出RejectedExecutionException异常。
+ * 2,ThreadPoolExecutor.DiscardPolicy：也是丢弃任务，但是不抛出异常。
+ * 3,ThreadPoolExecutor.DiscardOldestPolicy：丢弃队列最前面的任务，然后重新尝试执行任务（重复此过程）
+ * 4,ThreadPoolExecutor.CallerRunsPolicy：由调用线程处理该任务
+ */
+public class ThreadPoolRejectedDemo {
+    private static int THREADS_SIZE = 1;
+    private static int CAPACITY = 3;
+
+    /**
+     * abortPolicy:当线程的个数达到线程池的maximumPoolSize最大值时，如果等待队列已满则AbortPolicy
+     * 将会丢弃新任务并抛出RejectedExecutionException异常。
+     */
+    public static void abortPolicyDemo() {
+
+        // 创建线程池。线程池的"最大池大小"和"核心池大小"都为1(THREADS_SIZE)，"线程池"的阻塞队列容量为1(CAPACITY)。
+        ThreadPoolExecutor pool = new ThreadPoolExecutor(THREADS_SIZE, THREADS_SIZE, 0, TimeUnit.SECONDS,
+                new ArrayBlockingQueue<Runnable>(CAPACITY));
+        // 设置线程池的拒绝策略为"抛出异常"
+        pool.setRejectedExecutionHandler(new ThreadPoolExecutor.AbortPolicy());
+
+        try {
+            // 新建10个任务，并将它们添加到线程池中。
+            for (int i = 0; i < 10; i++) {
+                Runnable myrun = new MyRunnable("task-" + i);
+                pool.execute(myrun);
+            }
+        } catch (RejectedExecutionException e) {
+            e.printStackTrace();
+        } finally {
+            // 关闭线程池
+            pool.shutdown();
+        }
+    }
+
+    /**
+     * dicardPolicy和abortPolicy不同之处在于，dicardPolicy拒绝策略也会丢弃任务，但并不会抛出异常
+     */
+    public static void discardPolicyDemo() {
+
+        // 创建线程池。线程池的"最大池大小"和"核心池大小"都为1(THREADS_SIZE)，"线程池"的阻塞队列容量为1(CAPACITY)。
+        ThreadPoolExecutor pool = new ThreadPoolExecutor(THREADS_SIZE, THREADS_SIZE, 0, TimeUnit.SECONDS, new ArrayBlockingQueue<Runnable>(CAPACITY));
+        // 设置线程池的拒绝策略为"丢弃"
+        pool.setRejectedExecutionHandler(new ThreadPoolExecutor.DiscardPolicy());
+
+        // 新建10个任务，并将它们添加到线程池中。
+        for (int i = 0; i < 10; i++) {
+            Runnable myrun = new MyRunnable("task-" + i);
+            pool.execute(myrun);
+        }
+        // 关闭线程池
+        pool.shutdown();
+
+    }
+
+    /**
+     * DiscardOldestPolicy:当有任务添加到线程池被拒绝时，线程池会丢弃阻塞队列中末尾的任务，然后将被拒绝的任务添加到末尾(同步队列是先进先出的)。
+     */
+    public static void discardOldestPolicyDemo() {
+        // 创建线程池。线程池的"最大池大小"和"核心池大小"都为1(THREADS_SIZE)，"线程池"的阻塞队列容量为1(CAPACITY)。
+        ThreadPoolExecutor pool = new ThreadPoolExecutor(THREADS_SIZE, THREADS_SIZE, 0, TimeUnit.SECONDS,
+                new ArrayBlockingQueue<Runnable>(CAPACITY));
+        // 设置线程池的拒绝策略为"DiscardOldestPolicy"
+        pool.setRejectedExecutionHandler(new ThreadPoolExecutor.DiscardOldestPolicy());
+
+        // 新建10个任务，并将它们添加到线程池中。
+        for (int i = 0; i < 10; i++) {
+            Runnable myrun = new MyRunnable("task-" + i);
+            pool.execute(myrun);
+        }
+        // 关闭线程池
+        pool.shutdown();
+
+    }
+
+    /**
+     * 当有任务添加到线程池被拒绝时，线程池会将被拒绝的任务添加到”线程池正在运行的线程”中去运行。
+     */
+    public static void callerRunsPolicyDemo() {
+
+        // 创建线程池。线程池的"最大池大小"和"核心池大小"都为1(THREADS_SIZE)，"线程池"的阻塞队列容量为1(CAPACITY)。
+        ThreadPoolExecutor pool = new ThreadPoolExecutor(THREADS_SIZE, THREADS_SIZE, 0, TimeUnit.SECONDS,
+                new ArrayBlockingQueue<Runnable>(CAPACITY));
+        // 设置线程池的拒绝策略为"CallerRunsPolicy"
+        pool.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+
+        // 新建10个任务，并将它们添加到线程池中。
+        for (int i = 0; i < 10; i++) {
+            Runnable myrun = new MyRunnable("task-" + i);
+            pool.execute(myrun);
+        }
+
+        // 关闭线程池
+        pool.shutdown();
+
+    }
+
+
+    public static void main(String[] args) {
+        callerRunsPolicyDemo();
+    }
+
+
+}
--- src/main/java/com/concurrent/concurrent/threadpool/ThreadPoolUseDemo.java
+++ src/main/java/com/concurrent/concurrent/threadpool/ThreadPoolUseDemo.java
@@ -6,6 +6,22 @@ import java.util.concurrent.*;
 /**
  * 线程池的使用
  * ref:https://www.jianshu.com/p/210eab345423
+ * <p>
+ * 队列：
+ * SynchronousQueue：这个队列接收到任务的时候，会直接提交给线程处理，而不保留它，如果所有线程都在工作怎么办？
+ * 那就新建一个线程来处理这个任务！所以为了保证不出现<线程数达到了maximumPoolSize而不能新建线程>的错误，
+ * 使用这个类型队列的时候，maximumPoolSize一般指定成Integer.MAX_VALUE，即无限大
+ * <p>
+ * LinkedBlockingQueue：这个队列接收到任务的时候，如果当前线程数小于核心线程数，则新建线程(核心线程)处理任务；
+ * 如果当前线程数等于核心线程数，则进入队列等待。由于这个队列没有最大值限制，即所有超过核心线程数的任务都将被添加到队列中，
+ * 这也就导致了maximumPoolSize的设定失效，因为总线程数永远不会超过corePoolSize
+ * <p>
+ * ArrayBlockingQueue：可以限定队列的长度，接收到任务的时候，如果没有达到corePoolSize的值，则新建线程(核心线程)执行任务，
+ * 如果达到了，则入队等候，如果队列已满，则新建线程(非核心线程)执行任务，又如果总线程数到了maximumPoolSize，并且队列也满了
+ * ，则发生错误
+ * <p>
+ * DelayQueue：队列内元素必须实现Delayed接口，这就意味着你传进去的任务必须先实现Delayed接口。这个队列接收到任务时，
+ * 首先先入队，只有达到了指定的延时时间，才会执行任务
  */
 public class ThreadPoolUseDemo implements Runnable {
     protected int countDown = 10; //Default
